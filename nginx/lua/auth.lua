0ed62c74aa9c072c88c38eaf580a89135d5ad528
David Zuwenden <dhain@zognot.org>
2018-02-09 17:21:06 -0500

    Do away with proxying, just respond to auth_request

--- nginx/lua/auth.lua
+++ nginx/lua/auth.lua
@@ -8,8 +8,36 @@ local opts = {
     iat_slack = 600,
 }
 
+local oidc = require("resty.openidc")
+local res, err
+
+local path = ngx.var.request_uri
+path = path:match("(.-)%?") or path
+if path == os.getenv("OID_AUTH_PATH") then
+    res, err = oidc.access_token(opts)
+
+    if err then
+        ngx.status = 500
+        ngx.header.content_type = 'text/html';
+        ngx.say("There was an error while checking auth: " .. err)
+        ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+    end
+
+    if res == nil then
+        ngx.status = 401
+        ngx.header.content_type = 'text/html';
+        ngx.say("Unauthorized")
+        ngx.exit(ngx.HTTP_UNAUTHORIZED)
+    end
+
+    ngx.status = 202
+    ngx.header.content_type = 'text/html';
+    ngx.say("Accepted")
+    ngx.exit(ngx.HTTP_ACCEPTED)
+end
+
 -- call authenticate for OpenID Connect user authentication
-local res, err = require("resty.openidc").authenticate(opts)
+res, err = oidc.authenticate(opts)
 
 if err then
     ngx.status = 500
