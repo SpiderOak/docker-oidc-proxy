f8ac203a106359b77b993b127005ee5d26d21303
David Hain <dhain@zognot.org>
2018-02-10 18:09:23 -0500

    Add auth sub and email headers

--- nginx/lua/auth.lua
+++ nginx/lua/auth.lua
@@ -12,31 +12,6 @@ local opts = {
 local oidc = require("resty.openidc")
 local res, err, target_url
 
-local path = ngx.var.request_uri
-path = path:match("(.-)%?") or path
-if path == os.getenv("OID_AUTH_PATH") then
-    res, err = oidc.access_token(opts)
-
-    if err then
-        ngx.status = 500
-        ngx.header.content_type = 'text/html';
-        ngx.say("There was an error while checking auth: " .. err)
-        ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
-    end
-
-    if res == nil then
-        ngx.status = 401
-        ngx.header.content_type = 'text/html';
-        ngx.say("Unauthorized")
-        ngx.exit(ngx.HTTP_UNAUTHORIZED)
-    end
-
-    ngx.status = 202
-    ngx.header.content_type = 'text/html';
-    ngx.say("Accepted")
-    ngx.exit(ngx.HTTP_ACCEPTED)
-end
-
 -- call authenticate for OpenID Connect user authentication
 res, err, target_url = oidc.authenticate(opts)
 
@@ -46,6 +21,32 @@ if err then
 
     ngx.say("There was an error while logging in: " .. err)
     ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+end
+
+local path = target_url:match("(.-)%?") or target_url
+if path == os.getenv("OID_AUTH_PATH") then
+    if res == nil then
+        ngx.status = 401
+        ngx.header.content_type = 'text/html';
+        ngx.say("Unauthorized")
+        ngx.exit(ngx.HTTP_UNAUTHORIZED)
+    end
+
+    ngx.status = 202
+    ngx.header.content_type = 'text/html';
+
+    local sub_header = os.getenv("OID_AUTH_SUB_HEADER") or "X-SUB"
+    if sub_header then
+        ngx.header[sub_header] = res.id_token.sub
+    end
+
+    local email_header = os.getenv("OID_AUTH_EMAIL_HEADER") or "X-EMAIL"
+    if email_header then
+        ngx.header[email_header] = res.id_token.email
+    end
+
+    ngx.say("Accepted")
+    ngx.exit(ngx.HTTP_ACCEPTED)
 end
 
 local rd = target_url:match(".-%?rd=([^&]*)")
